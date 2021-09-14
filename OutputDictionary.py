8863e42fa0a2e5cc65654e463a0f8146b8b208ed
yamaji <crindigo0713@gmail.com>
2021-09-14 14:49:59 +0900

    spec5: Add word id

--- OutputDictionary.py
+++ OutputDictionary.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import numpy as np
 
 dictionary = pd.read_csv("dictionary.csv")
 
-for word in dictionary['単語']:
-    print(word)
+#単語idの追加
+dictionary["ID"] = range(1,len(dictionary)+1)
+
+for word_id in range(len(dictionary)):
+    id = dictionary["ID"][word_id]
+    word = dictionary["単語"][word_id]
+    print(str(id)+":"+word)
